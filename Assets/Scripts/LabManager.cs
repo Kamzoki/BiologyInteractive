02c7bea0a69bb43b48b5a48d1c7c46e7bf30a1f6
Kamzoki <kaittah@ymail.com>
2017-10-18 00:08:24 +0200

    Tool Class Finished, LabManager added

---
+++ Assets/Scripts/LabManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabManager : MonoBehaviour
+{
+
+    //private:
+    RaycastHit info;
+    Ray cameraRay;
+
+    //public:
+    public static LabManager LM;
+    [HideInInspector]
+    public GameObject currentSelectedObject;
+
+    private void Start()
+    {
+        LM = this;
+    }
+    // Update is called once per frame
+    /*   void Update()
+       {
+           CheckMouseClick();
+       }
+       */
+    public void CheckMouseClick()
+    {
+        //This function checks if the raycast fired from the mouse hit an object tagged tool or not.
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(cameraRay, out info))
+            {
+                if (info.collider.gameObject.tag == "Tool")
+                {
+                    currentSelectedObject = info.collider.gameObject;
+                }
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            currentSelectedObject = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Tool.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tool : MonoBehaviour {
+
+    //private:
+    float newXPosition;
+    float newYPosition;
+    float cameraDistance;
+
+    private void Start()
+    {
+        cameraDistance = (Camera.main.transform.position - gameObject.transform.position).magnitude;
+    }
+
+    private void OnMouseDrag()
+    {
+        LabManager.LM.CheckMouseClick();
+        if (gameObject.name == LabManager.LM.currentSelectedObject.name)
+        {
+            newXPosition = Input.mousePosition.x;
+            newYPosition = Input.mousePosition.y;
+            Vector3 mousePosition = new Vector3(newXPosition, newYPosition, cameraDistance);
+            Vector3 newObjPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            gameObject.transform.position = newObjPosition;
+        }
+    }
+}
