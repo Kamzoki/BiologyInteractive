242934f28ec0f9c4205c70dfa46e8f7ef239e497
Kamzoki <kaittah@ymail.com>
2017-12-30 19:21:48 +0200

    HumanPart for detecting clicks finished

---
+++ Assets/Scripts/HumanPart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanPart : MonoBehaviour {
+
+    public GameObject[] m_ActiavtionObjects;
+    public Animation m_AnimationComponent;
+    public string m_AnimationName;
+
+    private void ActivateObjects()
+    {
+        for (int i = 0; i < m_ActiavtionObjects.Length; i++)
+        {
+            m_ActiavtionObjects[i].SetActive(true);
+        }
+    }
+
+    private void PlayAnimation()
+    {
+        m_AnimationComponent.Play(m_AnimationName);
+    }
+    private void OnMouseUp()
+    {
+        ActivateObjects();
+        PlayAnimation();
+    }
+}
